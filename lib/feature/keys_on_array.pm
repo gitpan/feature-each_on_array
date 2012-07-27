033cfbcd029cd78f2f22916bcd3572c11ae235bf
Steven Haryanto <stevenharyanto@gmail.com>
2012-07-27 10:00:08 +0000

    Import of SHARYANTO/feature-each_on_array-0.01 from CPAN.
    gitpan-cpan-distribution: feature-each_on_array
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/feature-each_on_array-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/feature/keys_on_array.pm
@@ -0,0 +1,87 @@
+package feature::keys_on_array; # so as not to confuse dzil?
+our $VERSION = '0.01'; # VERSION
+use strict;
+use warnings;
+use feature::each_on_array ();
+use Carp;
+use Scalar::Util qw(reftype);
+
+package Tie::ArrayAsHash;
+
+sub akeys (\[@%]) {
+    my $thing = shift;
+    return keys %$thing
+        if reftype $thing eq 'HASH';
+    confess "should be passed a HASH or ARRAY"
+        unless reftype $thing eq 'ARRAY';
+
+    my $thing_h = $Tie::ArrayAsHash::cache{$thing} ||= do {
+        tie my %h, __PACKAGE__, $thing;
+        \%h
+    };
+
+    keys %$thing_h;
+}
+
+package feature::keys_on_array;
+
+sub import {
+    return unless $^V lt 5.12.0;
+    no strict 'refs';
+    my @caller = caller;
+    *{"$caller[0]::keys"} = \&akeys;
+}
+
+# XXX on unimport, delete symbol
+
+1;
+# ABSTRACT: Emulate keys(@array) on Perl < 5.12
+
+
+__END__
+=pod
+
+=head1 NAME
+
+feature::keys_on_array - Emulate keys(@array) on Perl < 5.12
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+ # This can run on Perls older than 5.12 and have no effect on 5.12+
+ use feature::keys_on_array;
+
+ my @a = (qw/a b c/);
+ my @keys = keys @a;
+
+=head1 DESCRIPTION
+
+Beginning with 5.12, Perl supports keys() on array. This module emulates the
+support on older Perls.
+
+=head1 CAVEATS
+
+Works on a per-package level, but does not work lexically yet.
+
+=head1 SEE ALSO
+
+L<feature::each_on_array>
+
+L<feature::values_on_array>
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
